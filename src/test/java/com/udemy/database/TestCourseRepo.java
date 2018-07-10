b4e9f24a6bb4ec4f8dd879b6ed9097b93607e7fa
Ali Mujthaba <a.mujthaba321@gmail.com>
2018-07-10 19:13:47 +0500

    added unit test for findbyID

---
+++ src/test/java/com/udemy/database/TestCourseRepo.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.udemy.database;
+
+import com.udemy.database.model.Course;
+import com.udemy.database.repository.CourseRepository;
+import static org.junit.Assert.*;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ *
+ * @author Ali Mujthaba
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestCourseRepo {
+    
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+    
+    @Autowired
+    private CourseRepository courseRepo;
+    
+    @Test
+    public void findById() {
+        
+        Course course = courseRepo.findById(1002);
+        assertEquals("Bachelor of Hibernate", course.getCourseName());
+        logger.info("Test is running");
+    
+    }
+    
+}
