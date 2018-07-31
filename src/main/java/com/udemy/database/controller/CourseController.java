88b34471894ff1368a826a40ff0f0491e20d0552
Ali Mujthaba <a.mujthaba321@gmail.com>
2018-07-31 19:36:38 +0500

    Added mockmvc tests

--- src/main/java/com/udemy/database/controller/CourseController.java
+++ src/main/java/com/udemy/database/controller/CourseController.java
@@ -4,6 +4,8 @@ import com.udemy.database.model.Course;
 
 import com.udemy.database.rest.repo.CourseRestRepostory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -28,6 +30,14 @@ public class CourseController {
         List<Course> courseList = new ArrayList<>();
         courseRestRepostory.findAll().forEach(courseList::add);
         return courseList;
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/myCourses/rest")
+    public ResponseEntity getCoursesListRest() {
+
+        List<Course> courseList = new ArrayList<>();
+        courseRestRepostory.findAll().forEach(courseList::add);
+        return new ResponseEntity<>(courseList, HttpStatus.OK);
     }
 
 }
--- src/test/java/com/udemy/database/CourseControllerTest.java
+++ src/test/java/com/udemy/database/CourseControllerTest.java
@@ -5,9 +5,17 @@ import com.udemy.database.model.Course;
 import com.udemy.database.rest.repo.CourseRestRepostory;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.experimental.results.ResultMatchers;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -30,6 +38,13 @@ public class CourseControllerTest {
     }
 
     @Test
+    public void testMockMvc() throws Exception {
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(courseController).build();
+        mockMvc.perform(MockMvcRequestBuilders.get("/myCourses/rest"))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
+    @Test
     public void getCourses() {
 
         List<Course> testCourses = new ArrayList<>();
@@ -37,9 +52,9 @@ public class CourseControllerTest {
         testCourses.add(new Course("My Course II"));
         Mockito.when(courseController.getCourses()).thenReturn(testCourses);
 
-
         List<Course> courseList = courseController.getCourses();
         assertEquals(2, courseList.size());
         assertEquals("My Course", courseList.get(0).getCourseName());
+        Mockito.verify(courseRestRepostory, Mockito.times(1)).findAll();
     }
 }
